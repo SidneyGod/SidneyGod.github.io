c58a676a85c32f4ad86bd516b8252de7f3d1c2f4
Sidney Ding <sidneyding183@gmail.com>
2022-12-25 21:24:24 +0800

    Add fold code feature
    Signed-off-by: Sidney Ding <sidneyding183@gmail.com>

--- README.md
+++ README.md
@@ -3,10 +3,4 @@
 To work locally with this project, you'll have to follow the steps below:
 
 1. Fork, clone or download this project
-1. [Install][] Hexo and [Hexo Server][hexo-server]
-1. Install dependencies: `npm install`
-1. Preview your site: `hexo server`
-1. Add content
-1. Generate your site (optional): `hexo generate`
-
-Read more at Hexo's [documentation][].
+1. Refer to: [Optimize hexo | Sidney God (sidneyding.cn)](https://sidneyding.cn/posts/ebc737d8/)
--- scripts/sidney_inject.js
+++ scripts/sidney_inject.js
@@ -6,8 +6,10 @@ const utils = new Util(hexo, __dirname);
 
 hexo.extend.filter.register('theme_inject', injects => {
   //injects.style.push(utils.getFilePath('../source/_data/sidney/sidney_style.styl'));
+  injects.style.push(utils.getFilePath('../source/_data/sidney/sidney_style.styl'));
 
-  injects.postBodyEnd.raw('sidney_postBodyEnd', utils.getFileContent('../source/_data/sidney/sidney_postBodyEnd.njk'));
+  injects.postBodyEnd.raw('sidney_postBodyEnd',
+        utils.getFileContent('../source/_data/sidney/sidney_postBodyEnd.njk'));
   injects.footer.raw('sidney_footer',
         utils.getFileContent('../source/_data/sidney/sidney_footer.njk'));
 
